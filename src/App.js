0eb6e06200bd1139f6d8b0e3e821f5aaace3cb30
Anthony Sanfilippo <anthony.sanfilippo@smartsheet.com>
2022-06-30 23:14:18 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,110 +1,132 @@
 /*
 Examples:
 https://dev.to/deboragaleano/how-to-handle-multiple-inputs-in-react-55el
-
+https://reactjs.org/docs/lifting-state-up.html
+https://reactjs.org/docs/handling-events.html
+https://www.geeksforgeeks.org/how-to-handle-multiple-input-field-in-react-form-with-a-single-function/
+https://medium.com/@jlangkammer/how-to-properly-log-state-change-in-react-588931f708f3
 */
 
-import React, { useState } from 'react';
-import { Button, FormGroup, Slider, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Button, FormGroup, Slider } from '@mui/material';
 
-const availableQuestionCount = 33;
-// let completedQuestionCount = 0;
 const sliderMin = 0;
 const sliderMax = 5;
 
-const CustomSlider = ({id, onChangeCommitted, value, defaultValue}) => (
-  <p> {questions[id].label}
+const CustomSlider = ({id, onChange}) => {
+  const { value } = useState();
+  return (<div>
+    <h1> {questions[id].label} </h1>
+    <p> {questions[id].text} </p>
     <Slider 
-    id={id}
-    min={sliderMin}
-    max={sliderMax}
-    marks={true}
-    valueLabelDisplay={"auto"}
-    onChangeCommitted={onChangeCommitted}
-    value={value}
-    defaultValue={defaultValue}
-  />
-  </p>
-   
-  
-    
-);
+      id={id}
+      name={id}
+      min={sliderMin}
+      max={sliderMax}
+      marks={true}
+      valueLabelDisplay={"auto"}
+      value={value}
+      onChange={onChange}
+    />
+  </div>)
+};
 
 
-function ProgressIndicator({completedQuestionCount, updateProgress}) {
-  // const [completedQuestionCount] = useState(0);
+function ProgressIndicator({completedQuestionCount, availableQuestionCount}) {
   return <p>{completedQuestionCount} out of {availableQuestionCount}</p>;
 };
 
-// function onSliderChange(event, newValue) {
-//   // event.preventDefault();
-//   // if(newValue === 0) {
-//   //   updateProgress(currentCompletedQuestionCount - 1);
-//   // } else {
-//   //   updateProgress(currentCompletedQuestionCount + 1);
-//   // }
-//   console.log()
+
+const questions = {
+  q1: { 
+    initialValue: 0, 
+    label: "Question 1",
+    text: "Is this question 1?"
+  },
+  q2: { 
+    initialValue: 0, 
+    label: "Question 2",
+    text: "Is this question 2?"
+  },
+  q3: { 
+    initialValue: 0, 
+    label: "Question 3",
+    text: "Is this question 3?"
+  },
+  q4: { 
+    initialValue: 0, 
+    label: "Question 4",
+    text: "Is this question 4?"
+  },
+  q5: { 
+    initialValue: 0, 
+    label: "Question 5",
+    text: "Is this question 5?"
+  },
+}
+
+
+// function getInitialValues() {
+//   let initialValues = {};
+//   for(let questionKey in questions) {
+//     initialValues[questionKey] = questions[questionKey].initialValue;
+//   }
+//   return initialValues;
 // }
 
-// const initialValues = {
-//   q1: "",
-//   q2: "",
-//   q3: "",
-//   q4: "",
-//   q5: "",
-// };
-
-/*
-NOTE: Value cannot be set on first render or else this error shows up:
-
-MUI: A component is changing the controlled value state of Slider to be uncontrolled.
-Elements should not switch from uncontrolled to controlled (or vice versa).
-Decide between using a controlled or uncontrolled Slider element for the lifetime of the component.
-The nature of the state is determined during the first render. It's considered controlled if the value is not `undefined`.
-*/
-const questions = {
-  q1: { initialValue: undefined, label: "Question 1" },
-  q2: { initialValue: undefined, label: "Question 2" },
-  q3: { initialValue: undefined, label: "Question 3" },
-  q4: { initialValue: undefined, label: "Question 4" },
-  q5: { initialValue: undefined, label: "Question 5" },
-}
-
-function getInitialValues() {
-  let initialValues = {};
-  for(let questionKey in questions) {
-    initialValues[questionKey] = questions[questionKey].initialValue;
-  }
-  return initialValues;
-}
-
 export default function App() {
-  const [currentCompletedQuestionCount, updateProgress] = useState(0);
+  const [answeredQuestions, setValues] = useState({}); // could use getInitialValue instead of {}
 
-  const [values, setValues] = useState(getInitialValues);
+  // NOTE!!: the MaterialUI slider does not pass the ID value to the event 
+  // for some stupid fucking reason, it didn't seem to be listed in the api docs
+  // https://stackoverflow.com/questions/53611848/how-do-i-identify-a-material-ui-slider-in-react
 
-  const handleInputChange = (e) => {
-    const { id, value } = e.target;
-    setValues({
-      ...values,
-      [id]: value,
-    });
-    console.log(values)
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setValues({...answeredQuestions, [name]: value});
   };
+
+  
+
+  // log output
+  // useEffect(() => {
+  //   console.log(values);
+  // }, [values]);
+
+  let completedQuestionCount = Object.keys(answeredQuestions).length || 0;
+  const availableQuestionCount = Object.keys(questions).length || 0;
 
   return (
     <form style={{width:600, marginLeft:'20%'}}>
       <ProgressIndicator
-        completedQuestionCount={currentCompletedQuestionCount}
-        updateProgress={updateProgress}
+        completedQuestionCount={completedQuestionCount}
+        availableQuestionCount={availableQuestionCount}
       />
       <FormGroup >
-        <CustomSlider id='q1' defaultValue={5} value={values.q1} onChangeCommitted={handleInputChange} />
-        <CustomSlider id='q2' value={values.q2} onChangeCommitted={handleInputChange} />
-        <CustomSlider id='q3' value={values.q3} onChangeCommitted={handleInputChange} />
-        <CustomSlider id='q4' value={values.q4} onChangeCommitted={handleInputChange} />
+        <CustomSlider 
+          id='q1'
+          value={answeredQuestions?.q1 || 0}
+          onChange={handleInputChange}
+        />
+        <CustomSlider
+          id='q2'
+          value={answeredQuestions?.q2 || 0}
+          onChange={handleInputChange}
+        />
+        <CustomSlider
+          id='q3'
+          value={answeredQuestions?.q3 || 0}
+          onChange={handleInputChange}
+        />
+        <CustomSlider
+          id='q4'
+          value={answeredQuestions?.q4 || 0}
+          onChange={handleInputChange}
+        />
       </FormGroup>
-      <Button type="submit"> Submit </Button>
+      <Button onClick={()=>(console.log(answeredQuestions))}> Debug </Button>
+
+      <Button type="primary" onClick={()=>(console.log(answeredQuestions))}> Submit </Button>
       
     </form>
   );
